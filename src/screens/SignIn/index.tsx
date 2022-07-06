cc79b75f34e8d9839fcda4360bf20c51d5681615
pedrokpdo <pedromotoehm@gmail.com>
2022-07-06 13:56:06 -0300

    Finalizando signIn

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,32 +1,39 @@
 import React from 'react'
-import { Container } from './styles'
+import { Container, Content, Title, Brand, ForgotPasswordButton, ForgotPasswordLabel } from './styles'
+import brandImg from '../../assets/brand.png'
 import { Input } from '../../components/input'
 import { Button } from '../../components/Buttom'
-import { KeyboardAvoidingView } from 'react-native'
+import { KeyboardAvoidingView, Platform } from 'react-native'
 
 
 export function SignIn() {
 
     return (
         <Container>
-            <KeyboardAvoidingView>
-
-                <Input
-                    placeholder='E-mail'
-                    type='secondary'
-                    autoCorrect={false}
-                    autoCapitalize='none'
-                />
-                <Input
-                    placeholder='E-mail'
-                    type='secondary'
-                    autoCorrect={false}
-                    autoCapitalize='none'
-                />
-                <Button
-                    title='Entrar'
-                    type='secondary'
-                />
+            <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+                <Content>
+                    <Brand source={brandImg} />
+                    <Title>LOGIN</Title>
+                    <Input
+                        placeholder='E-mail'
+                        type='secondary'
+                        autoCorrect={false}
+                        autoCapitalize='none'
+                    />
+                    <Input
+                        placeholder='E-mail'
+                        type='secondary'
+                        autoCorrect={false}
+                        autoCapitalize='none'
+                    />
+                    <ForgotPasswordButton>
+                        <ForgotPasswordLabel>Esqueci minha Senha</ForgotPasswordLabel>
+                    </ForgotPasswordButton>
+                    <Button
+                        title='Entrar'
+                        type='secondary'
+                    />
+                </Content>
             </KeyboardAvoidingView>
 
         </Container>
